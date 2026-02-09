e75b992cb286e8f9c6edbc367379fef58d02435a
ramailo1 <ramailorakou@gmail.com>
2026-02-09 16:09:03 +0100

    FajerShow Blocked by Cloudflare

--- FajerShowProvider/build.gradle.kts
+++ FajerShowProvider/build.gradle.kts
@@ -2,7 +2,7 @@ version = 10
 
 cloudstream {
     authors = listOf( "ramailo1" )
-    description = "FajerShow Provider - (In Progress)"
+    description = "FajerShow Provider - (Blocked by cloudflare)"
 
     language = "ar"
 	
